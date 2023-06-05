c292819eb35f0cadaf4a5f38431898bdf031c1b0
agamjotsingh18 <70067726+agamjotsingh18@users.noreply.github.com>
2023-06-05 12:10:41 +0530

    updated

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import MemeGenerator from './MemeGenerator'
+
+
+function App() {
+  return (
+    <div className="App">
+      <MemeGenerator />
+    </div>
+  );
+}
+
+export default App;
